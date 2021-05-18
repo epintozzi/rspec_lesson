e34dcb758b6fc89c0ed34be73168799397ce86c2
Erin Pintozzi <erin.pintozzi@gmail.com>
2021-05-18 00:25:27 -0700

    update dog default values syntax

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
 class Dog
-  attr_reader :name, :color, :toys
+  attr_reader :name, :color, :toys, :owner
 
-  def initialize(name, color = 'brown')
+  def initialize(name, color: 'brown', owner: 'stray')
     @name = name
     @color = color
     @toys = []
+    @owner = owner
   end
 
   def find_toy(toy)
